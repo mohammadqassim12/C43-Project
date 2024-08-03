60dcdf8c6e3454d195c52690b9104e437dcb00e5
Mohammad Al-Qasem <mohammad.alqassim@mail.utoronto.ca>
2024-08-02 22:10:07 -0400

    remove the app

--- portfolio.h
+++
@@ -1,9 +0,0 @@
-#ifndef PORTFOLIO_H
-#define PORTFOLIO_H
-
-#include <pqxx/pqxx>
-
-void createPortfolio(pqxx::work& W, int userID, double cashAmount);
-void viewPortfolios(pqxx::nontransaction& N, int userID);
-
-#endif
--- review.h
+++
@@ -1,9 +0,0 @@
-#ifndef REVIEW_H
-#define REVIEW_H
-
-#include <pqxx/pqxx>
-
-void addReview(pqxx::work& W, int userID, const std::string& listName, const std::string& text);
-void viewReviews(pqxx::nontransaction& N, const std::string& listName);
-
-#endif
